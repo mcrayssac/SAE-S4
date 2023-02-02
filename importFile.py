604772fddb01b9bbc23b7a3777965f149667872e
mcrayssac <mcrayssa@iut-bm.univ-fcomte.fr>
2023-02-02 09:33:21 +0100

    Updated importFile.py

--- importFile.py
+++ importFile.py
@@ -2,12 +2,19 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
+from pandas.io.json import json_normalize
 
 #print(pd.show_versions())
 
 #----- Import File -----
-df_vaccin = pd.read_csv('data.csv', index_col="YearWeekISO")
+#df_vaccin = pd.read_csv('data.csv')
+print("Beginning")
+url = "https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/json/"
+df_vaccin = pd.read_json(url)
 df_vaccin.sort_index()
+print(df_vaccin)
+print("Ending")
 
-df_contamination = pd.read_csv('data_conta.csv', index_col="year_week")
-df_contamination.sort_index()
+#df_contamination = pd.read_csv('data_conta.csv', index_col="year_week")
+#df_contamination.sort_index()
