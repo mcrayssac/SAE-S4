8cb676db1806c61895a23957454c740a89c40b4b
aurelcode <63055821+aurelcode@users.noreply.github.com>
2023-02-02 09:41:17 +0100

    cleaning en cours

--- Contamination.py
+++ Contamination.py
@@ -1,3 +1,5 @@
+import itertools
+
 import importFile
 
 sns = importFile.sns
@@ -6,11 +8,32 @@ pd = importFile.pd
 np = importFile.np
 df_contamination = importFile.df_contamination
 
-fig2 = plt.figure(figsize=(35,25))
-sns.lineplot(data=df_contamination, x="year_week", y="cumulative_count")
-plt.xlabel("date as year and week")
-plt.ylabel("number of death")
-plt.title("evolution of the number of death through the world in from the end of 2020 to 2023")
-plt.xticks(rotation=90)
-plt.grid()
-plt.show()
+print(df_contamination.isnull().sum().sum())
+
+def globalContaByWeek():
+    plt.figure(figsize=(35,25))
+    sns.lineplot(data=df_contamination, x="year_week", y="cumulative_count")
+    plt.xlabel("date as year and week")
+    plt.ylabel("number of contamination")
+    plt.title("evolution of the number of cases through the world from the end of 2020 to 2023")
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.show()
+
+def evolutionContaminationRateByWeek(country):
+    plt.figure(figsize=(35, 25))
+    df_contamination_country = df_contamination["country"] == country
+    sns.lineplot(data=df_contamination[df_contamination_country], x="year_week", y="weekly_count")
+    plt.xlabel("date as year and week")
+    plt.ylabel("number of contamination")
+    plt.title("evolution of the number of cases in "+country+" from the end of 2020 to 2023")
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.show()
+
+
+
+# execute
+#globalContaByWeek()
+#evolutionContaminationRateByWeek('Africa (total)')
+#evolutionContaminationRateByWeek('France')
--- importFile.py
+++ importFile.py
@@ -2,8 +2,6 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import requests
-from pandas.io.json import json_normalize
 
 #print(pd.show_versions())
 
@@ -16,5 +14,20 @@ df_vaccin.sort_index()
 print(df_vaccin)
 print("Ending")
 
-#df_contamination = pd.read_csv('data_conta.csv', index_col="year_week")
-#df_contamination.sort_index()
+print("beginning cleaning")
+#df_vaccin['NumberDosesReceived'] = df_vaccin['NumberDosesReceived'].replace('', np.nan)
+#df_contamination = df_vaccin.dropna(subset=['NumberDosesReceived'])
+print(df_vaccin.isnull().sum())
+# les champs qui contiennent des nulls sont : denominator, numberDoseReceived, numberDoseExported et firstDoseRefused
+print("###################################################################################################################")
+df_contamination = pd.read_csv('data_conta.csv')
+df_contamination.sort_index()
+df_contamination.pop('note')
+df_contamination['weekly_count'] = df_contamination['weekly_count'].replace('', np.nan)
+df_contamination = df_contamination.dropna(subset=['weekly_count'])
+df_contamination['rate_14_day'] = df_contamination['rate_14_day'].replace('', np.nan)
+df_contamination = df_contamination.dropna(subset=['rate_14_day'])
+df_contamination['weekly_count'] = df_contamination['weekly_count'].replace('', np.nan)
+print(df_contamination.isnull().sum()) # les champs null qu'il reste sont ceux des pays avec (total) dans leur nom donc
+# a voir ce qu'on fait si on leur trouve une abreviation ou autre chose
+#print(df_contamination)
