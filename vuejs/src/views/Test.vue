efc30bca6c21da793ae7743fa1b0e87bf09f085b
Maxime Crayssac <ml.crayssac@gmail.com>
2023-03-02 15:07:01 +0100

    Update server.js and Test.vue

--- vuejs/src/views/Test.vue
+++ vuejs/src/views/Test.vue
@@ -8,14 +8,43 @@
       </v-row>
     </section>
 
-    <section class="Graph">
-      <JSCharting :options="chartOptions" class="columnChart"/>
-    </section>
+    <v-banner color="#5F7174" rounded elevation="6">
+      <v-row>
+        <v-col cols="auto" align-self="center">
+          <v-icon color="#32D9CB" size="36">
+            mdi-poll
+          </v-icon>
+        </v-col>
+        <v-col class="pb-1" cols="auto" align-self="center">
+          <v-tooltip bottom color="#2374AB">
+            <template v-slot:activator="{ on, attrs }">
+              <span v-bind="attrs" v-on="on" class="select-bar">
+                  Here our title
+              </span>
+            </template>
+            <span class="tooltip-text">Boutique sélectionnée</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+    </v-banner>
+
+    <v-banner class="mt-5" color="#5F7174" rounded elevation="6">
+      <v-row>
+        <v-col cols="auto" align-self="center">
+          <section class="Graph">
+            <JSCharting :options="chartOptions"/>
+          </section>
+        </v-col>
+      </v-row>
+    </v-banner>
+
+
   </v-container>
 </template>
 
 <script>
 import JSCharting from 'jscharting-vue';
+
 export default {
   name: 'Test',
   data: () => ({
@@ -24,9 +53,9 @@ export default {
       series: [
         {
           points: [
-            { x: 'A', y: 50 },
-            { x: 'B', y: 30 },
-            { x: 'C', y: 50 }
+            {x: 'A', y: 50},
+            {x: 'B', y: 30},
+            {x: 'C', y: 50}
           ]
         }
       ]
@@ -37,3 +66,21 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.tooltip-text{
+  color: #D6FFF6 !important;
+  font-size: 1em !important;
+  font-family: "Montserrat Medium" !important;
+  font-weight: bold !important;
+  letter-spacing: 2px !important;
+}
+
+.select-bar{
+  color: #A5E65A !important;
+  font-size: 2em !important;
+  font-family: "Montserrat Medium" !important;
+  font-weight: bold !important;
+  letter-spacing: 2px !important;
+}
+</style>
