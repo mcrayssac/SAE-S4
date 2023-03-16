813ff1838d1bdf317c894c61210301eb7269d0b1
aurelcode <aurelien.guillou@edu.univ-fcomte.fr>
2023-03-16 11:05:06 +0100

    amelioration affichage modele SIR

--- API/Service/GraphService.js
+++ API/Service/GraphService.js
@@ -369,14 +369,27 @@ async function prediction(country, transmission, duration){
         removed: removed0,
         notSick: notSick0,
         population: population0});
-    let year = results[0].YearWeekISO.split('-')[0];
-    let week = Number(results[0].YearWeekISO.split('-W')[1]);
+    let year = Number(results[0].YearWeekISO.split('-')[0]);
+    let week = 0;
+    let weekAdd = Number(results[0].YearWeekISO.split('-W')[1]);
     let i = 1;
-    while(i <= 20 && notSick0 > 0 && removed0 > 0 && sick0 > 0){
-        let YearWeekISO = year+"-W"+(week+i);
+    let YearWeekISO = null;
+    let stagnation = 0;
+    while(notSick0 > 0 && removed0 > 0 && sick0 > 0 && stagnation < 10){
+        if(i <= 52){
+            YearWeekISO = year+"-W"+(weekAdd+i);
+        }
+        else{
+            year=year+1;
+            i = 1;
+            YearWeekISO = year+"-W"+(week+i);
+        }
         let infected= Math.round(sick0 + ((transmission*sick0*notSick0 - duration*sick0)/population0));
         let removed= Math.round(removed0 + (duration*sick0/population0));
         let notSick = Math.round(notSick0 - (transmission*sick0*notSick0/population0));
+        if(infected === notSick0 || notSick === notSick0 ){
+            stagnation++;
+        }
         infected = infected > 0 ? infected : 0;
         removed = removed > 0 ? removed : 0;
         notSick = notSick > 0 ? notSick : 0;
